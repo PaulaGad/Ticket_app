eee41af6f890da7ba6c68bb48a2ac49aab4bb7a7
Paula Gadomska <paulagadomska@wp.pl>
2021-06-07 18:12:24 +0200

    Fixed problem with props in styled-component

--- src/components/Seats/SeatOutlook/SeatOutlook.js
+++ src/components/Seats/SeatOutlook/SeatOutlook.js
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const SeatOutlook = styled.div`
-  border: 1px solid darkblue;
-  height: calc(20px + 2vmin);
-  width: calc(20px + 2vmin);
-  grid-column-start: ${props => props.y + 1};
-  grid-row-start: ${props => props.x + 1};
-  background-color: ${props => props.color};
-  @media (max-width: 1200px) {
-    height: calc(10px + 2vmin);
-    width: calc(10px + 2vmin);
-  }
-`
+const SeatOutlook = styled.div.attrs(props => ({
+  style: {
+    gridColumnStart: props.y + 1,
+    gridRowStart: props.x + 1,
+    backgroundColor: props.color
+  },
+}))`border: 1px solid darkblue;
+height: calc(20px + 2vmin);
+width: calc(20px + 2vmin);
+@media (max-width: 1200px) {
+  height: calc(10px + 2vmin);
+  width: calc(10px + 2vmin);
+}`;
 
 SeatOutlook.defaultProps = {
   color: 'white',
